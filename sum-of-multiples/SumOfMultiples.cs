586df1237fd40bd3a33a6049721ab48ed2fffa91
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-05-09 22:40:11 -0600

    Add implementation for sum of multiples

--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var uniqueMultiples = new HashSet<int>();
+        foreach (var multiple in multiples)
+        {
+            uniqueMultiples.UnionWith(getAllPositiveMultiples(multiple).TakeWhile(x => x < max));
+        }
+        return uniqueMultiples.Sum();
+    }
+
+    private static IEnumerable<int> getAllPositiveMultiples(int baseValue)
+    {
+        for (var i = 1; i < int.MaxValue; i++) {
+            yield return baseValue * i;
+        }
     }
 }
