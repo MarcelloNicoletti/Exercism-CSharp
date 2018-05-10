75dd892271e166f3bf89d0b76fc549bbfad0dd41
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-05-09 23:09:42 -0600

    Add implementation for raindrops

--- raindrops/Raindrops.cs
+++ raindrops/Raindrops.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Text;
 
 public static class Raindrops
 {
+    private const string Pling = "Pling";
+    private const string Plang = "Plang";
+    private const string Plong = "Plong";
+
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var soundBuilder = new StringBuilder();
+        if (number % 3 == 0)
+        {
+            soundBuilder.Append(Pling);
+        }
+        if (number % 5 == 0)
+        {
+            soundBuilder.Append(Plang);
+        }
+        if (number % 7 == 0)
+        {
+            soundBuilder.Append(Plong);
+        }
+        if (soundBuilder.Length == 0)
+        {
+            soundBuilder.Append(number);
+        }
+
+        return soundBuilder.ToString();
     }
 }
