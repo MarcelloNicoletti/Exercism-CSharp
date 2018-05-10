01a7f5bef8f613850f3609ffad41189f24495ff1
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-05-09 21:27:19 -0600

    Add implementation for bob

--- bob/Bob.cs
+++ bob/Bob.cs
@@ -4,6 +4,42 @@ public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (string.IsNullOrEmpty(statement.Trim())) {
+            return "Fine. Be that way!";
+        }
+        if (IsShouting(statement.Trim()))
+        {
+            if (IsAsking(statement.Trim()))
+            {
+                return "Calm down, I know what I'm doing!";
+            }
+            return "Whoa, chill out!";
+        }
+        if (IsAsking(statement.Trim()))
+        {
+            return "Sure.";
+        }
+        return "Whatever.";
+    }
+
+    private static bool IsShouting(string statement)
+    {
+        var hasLetters = false;
+        for (int i = 0; i < statement.Length; i++)
+        {
+            if (Char.IsLetter(statement[i]))
+            {
+                hasLetters = true;
+                if (!Char.IsUpper(statement[i])) {
+                    return false;
+                }
+            }
+        }
+        return hasLetters;
+    }
+
+    private static bool IsAsking(string statement)
+    {
+        return statement[statement.Length - 1] == '?';
     }
 }
