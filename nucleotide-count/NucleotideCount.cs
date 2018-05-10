874b57888c864bc9d2b46225c57c9a90701c8c3e
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-05-10 13:38:00 -0600

    Add implementation for nucleotide count

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -1,18 +1,40 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 public class NucleotideCount
 {
+    private static readonly List<char> ValidNucleotides = new List<char> {'A', 'C', 'G', 'T'};
+
+    private readonly IDictionary<char, int> _nucleotideCounts;
+
     public NucleotideCount(string sequence)
     {
+        var nucleotideCounts = countNucleotides(sequence);
+        _nucleotideCounts = new ReadOnlyDictionary<char, int>(nucleotideCounts);
     }
 
     public IDictionary<char, int> NucleotideCounts
     {
-        get
+        get { return _nucleotideCounts; }
+    }
+
+    private IDictionary<char, int> countNucleotides(string sequence)
+    {
+        var nucleotideCounts = new Dictionary<char, int>();
+        ValidNucleotides.ForEach(x => nucleotideCounts.Add(x, 0));
+
+        foreach (var nucleotide in sequence)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (!ValidNucleotides.Contains(nucleotide))
+            {
+                throw new InvalidNucleotideException();
+            }
+
+            nucleotideCounts[nucleotide] += 1;
         }
+
+        return nucleotideCounts;
     }
 }
 
