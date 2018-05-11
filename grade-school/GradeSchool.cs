6e562ca81e10cd180f9922df99577e2875da604a
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-05-10 21:15:45 -0600

    Add implementation for grade school

--- grade-school/GradeSchool.cs
+++ grade-school/GradeSchool.cs
@@ -1,20 +1,39 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public class School
 {
+    private readonly IDictionary<int, ISet<string>> _roster = new SortedDictionary<int, ISet<string>>();
+
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!_roster.ContainsKey(grade))
+        {
+            _roster.Add(grade, new SortedSet<string>());
+        }
+
+        _roster[grade].Add(student);
     }
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        foreach (var grade in _roster.Keys)
+        {
+            foreach (var student in _roster[grade])
+            {
+                yield return student;
+            }
+        }
     }
 
     public IEnumerable<string> Grade(int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!_roster.ContainsKey(grade))
+        {
+            return Enumerable.Empty<string>();
+        }
+
+        return _roster[grade];
     }
 }
