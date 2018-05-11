dced21fedbe090aa81fab191dbb1ec24e7e44ee0
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-05-10 20:23:58 -0600

    Add implementation for accumulate

--- accumulate/Accumulate.cs
+++ accumulate/Accumulate.cs
@@ -5,6 +5,10 @@ public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var accumulated = new List<U>();
+        foreach (var item in collection)
+        {
+            yield return func(item);
+        }
     }
 }
