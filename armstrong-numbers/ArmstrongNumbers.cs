914bf62916e7e8e2b33ab6f9c3209ceeff7ee330
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-05-11 17:20:22 -0600

    Add implementation for armstrong numbers

--- armstrong-numbers/ArmstrongNumbers.cs
+++ armstrong-numbers/ArmstrongNumbers.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var remainingDigits = number;
+        var digits = new List<int>();
+        while (remainingDigits > 0)
+        {
+            digits.Add(remainingDigits % 10);
+            remainingDigits /= 10;
+        }
+
+        var numDigits = digits.Count;
+        return number == digits.Select(x => Math.Pow(x, numDigits)).Sum();
     }
 }
