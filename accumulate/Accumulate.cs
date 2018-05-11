c7de2f4b13bbfdef028e73d852d60db1450a83e4
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-05-10 20:46:34 -0600

    Refactor for accumulate

--- accumulate/Accumulate.cs
+++ accumulate/Accumulate.cs
@@ -5,7 +5,6 @@ public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        var accumulated = new List<U>();
         foreach (var item in collection)
         {
             yield return func(item);
