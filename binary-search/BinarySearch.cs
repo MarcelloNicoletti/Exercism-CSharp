aff8633d4000673b40e134624b8dca4de0715e14
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-05-21 17:51:21 -0600

    Add implementation for binary search

---
+++ binary-search/BinarySearch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class BinarySearch
+{
+    private readonly int[] _values;
+
+    public BinarySearch (int[] input)
+    {
+        _values = new int[input.Length];
+        Array.Copy(input, _values, input.Length);
+    }
+
+    public int Find (int value)
+    {
+        if (_values.Length == 0)
+        {
+            return -1;
+        }
+
+        var start = 0;
+        var end = _values.Length - 1;
+        while (true)
+        {
+            var middle = (start + end) / 2;
+            var guess = _values[middle];
+            if (start == middle || end == middle)
+            {
+                return guess == value ? middle : -1;
+            }
+
+            if (guess == value)
+            {
+                return middle;
+            }
+
+            if (value < guess)
+            {
+                end = middle - 1;
+            }
+            else
+            {
+                start = middle + 1;
+            }
+        }
+    }
+}
