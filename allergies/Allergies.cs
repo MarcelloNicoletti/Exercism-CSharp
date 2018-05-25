ca126c31fe201b88efb62f08f7a6684bc64a3990
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-05-25 15:53:14 -0600

    Add implementation for allergies exercise

--- allergies/Allergies.cs
+++ allergies/Allergies.cs
@@ -1,19 +1,80 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Allergies
 {
-    public Allergies(int mask)
+    private readonly AllergyEnum _flags;
+
+    public Allergies (int mask)
     {
+        _flags = (AllergyEnum)mask;
     }
 
-    public bool IsAllergicTo(string allergy)
+    public bool IsAllergicTo (string allergy)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return _flags.Has(allergy);
     }
 
-    public IList<string> List()
+    public IList<string> List ()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return _flags.GetAllergyList();
+    }
+}
+
+[Flags]
+public enum AllergyEnum
+{
+    Eggs = 1 << 0,
+    Peanuts = 1 << 1,
+    Shellfish = 1 << 2,
+    Strawberries = 1 << 3,
+    Tomatoes = 1 << 4,
+    Chocolate = 1 << 5,
+    Pollen = 1 << 6,
+    Cats = 1 << 7
+}
+
+public static class AllergyEnumExtensions
+{
+    private static readonly IDictionary<AllergyEnum, string> AllergyEnumToString = new Dictionary<AllergyEnum, string>
+    {
+        {AllergyEnum.Eggs, "eggs"},
+        {AllergyEnum.Peanuts, "peanuts"},
+        {AllergyEnum.Shellfish, "shellfish"},
+        {AllergyEnum.Strawberries, "strawberries"},
+        {AllergyEnum.Tomatoes, "tomatoes"},
+        {AllergyEnum.Chocolate, "chocolate"},
+        {AllergyEnum.Pollen, "pollen"},
+        {AllergyEnum.Cats, "cats"}
+    };
+
+    private static readonly IDictionary<string, AllergyEnum> AllergyStringToEnum =
+        AllergyEnumToString.ToDictionary(entry => entry.Value, entry => entry.Key);
+
+    public static bool Has (this AllergyEnum flags, string allergy)
+    {
+        if (!AllergyStringToEnum.ContainsKey(allergy))
+        {
+            return false;
+        }
+
+        var flag = AllergyStringToEnum[allergy];
+        return flags.HasFlag(flag);
+    }
+
+    public static IList<string> GetAllergyList (this AllergyEnum flags)
+    {
+        var allergies = new List<string>();
+
+        foreach (AllergyEnum allergy in Enum.GetValues(typeof(AllergyEnum)))
+        {
+            if (flags.HasFlag(allergy))
+            {
+                allergies.Add(AllergyEnumToString[allergy]);
+            }
+        }
+
+        return allergies;
     }
 }
