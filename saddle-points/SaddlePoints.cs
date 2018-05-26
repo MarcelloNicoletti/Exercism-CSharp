c7c8a5deaae9c42b62591bbeed0697868ff1f8bf
MarcelloNicoletti <marcello.s.nicoletti@gmail.com>
2018-05-26 02:17:36 -0600

    Refactor saddle points

--- saddle-points/SaddlePoints.cs
+++ saddle-points/SaddlePoints.cs
@@ -4,47 +4,73 @@ using System.Linq;
 
 public class SaddlePoints
 {
-    private readonly int[,] _matrix;
+    private readonly Matrix<int> _matrix;
 
     public SaddlePoints (int[,] values)
     {
-        _matrix = values;
+        _matrix = new Matrix<int>(values);
     }
 
     public IEnumerable<Tuple<int, int>> Calculate ()
     {
-        var rowValues = new int[_matrix.GetLength(0)][];
-        var colValues = new int[_matrix.GetLength(1)][];
-        CacheRowsAndCols(rowValues, colValues);
+        var maxForRow = Memoizer.Memoize((int x) => _matrix.GetRow(x).Max());
+        var minForColumn = Memoizer.Memoize((int y) => _matrix.GetColumn(y).Min());
 
-        for (var row = 0; row < rowValues.Length; row++)
+        for (var row = 0; row < _matrix.NumRows; row++)
         {
-            for (var col = 0; col < colValues.Length; col++)
+            for (var col = 0; col < _matrix.NumCols; col++)
             {
-                var val = _matrix[row, col];
-                if (rowValues[row].All(x => x <= val) && colValues[col].All(y => y >= val))
+                if (maxForRow(row) == _matrix[row, col] && minForColumn(col) == _matrix[row, col])
                 {
                     yield return (row, col).ToTuple();
                 }
             }
         }
     }
+}
 
-    private void CacheRowsAndCols (IList<int[]> rows, IList<int[]> cols)
+internal class Matrix<T>
+{
+    private readonly T[,] _matrix;
+
+    public Matrix (T[,] values)
     {
-        for (var row = 0; row < rows.Count; row++)
-        {
-            rows[row] = new int[cols.Count];
-            for (var col = 0; col < cols.Count; col++)
-            {
-                if (row == 0)
-                {
-                    cols[col] = new int[rows.Count];
-                }
+        _matrix = (T[,])values.Clone();
+    }
 
-                rows[row][col] = _matrix[row, col];
-                cols[col][row] = _matrix[row, col];
+    public int NumRows => _matrix.GetLength(0);
+    public int NumCols => _matrix.GetLength(1);
+    public T this [int row, int col] => _matrix[row, col];
+
+    public IEnumerable<T> GetRow (int row)
+    {
+        return Enumerable.Range(0, NumCols).Select(col => this[row, col]);
+    }
+
+    public IEnumerable<T> GetColumn (int col)
+    {
+        return Enumerable.Range(0, NumRows).Select(row => this[row, col]);
+    }
+}
+
+internal static class Memoizer
+{
+    public static Func<T, TResult> Memoize<T, TResult> (Func<T, TResult> func)
+    {
+        var map = new Dictionary<T, TResult>();
+        return x =>
+        {
+            if (!map.ContainsKey(x))
+            {
+                map[x] = func(x);
             }
-        }
+
+            return map[x];
+        };
+    }
+
+    public static Func<T, TResult> AsMemoized<T, TResult> (this Func<T, TResult> func)
+    {
+        return Memoize(func);
     }
 }
